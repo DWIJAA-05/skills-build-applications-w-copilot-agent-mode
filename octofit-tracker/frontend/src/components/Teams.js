aba8af5a1570b59f693309d5c10c6f844c156dfc
DWIJAA-05 <kiran.pawar@parentpay.com>
2025-04-08 09:28:38 +0000

    Commit changes

---
+++ octofit-tracker/frontend/src/components/Teams.js
@@ -0,0 +1,34 @@
+import React, { useEffect, useState } from 'react';
+
+function Teams() {
+  const [teams, setTeams] = useState([]);
+
+  useEffect(() => {
+    fetch('https://effective-space-broccoli-5grg979xq67wc7r96-8000.app.github.dev/api/teams/')
+      .then(response => response.json())
+      .then(data => setTeams(data))
+      .catch(error => console.error('Error fetching teams:', error));
+  }, []);
+
+  return (
+    <div>
+      <h1 className="text-center">Teams</h1>
+      <table className="table table-striped">
+        <thead>
+          <tr>
+            <th>Team Name</th>
+          </tr>
+        </thead>
+        <tbody>
+          {teams.map(team => (
+            <tr key={team._id}>
+              <td>{team.name}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+}
+
+export default Teams;
